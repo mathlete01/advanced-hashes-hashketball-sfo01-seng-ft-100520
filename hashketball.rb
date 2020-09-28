77c1e36433e93a70189f1ee21e9ba3a6091eb234
Matthew Sallin <msallin@gmail.com>
2020-09-28 22:29:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,8 +185,8 @@ def player_stats(name)
     team_data[:players][num][:player_name]
       if team_data[:players][num][:player_name] == name
         answer = team_data[:players][num]
-        return answer
         binding.pry
+        return answer
       num += 1
     end
   end
