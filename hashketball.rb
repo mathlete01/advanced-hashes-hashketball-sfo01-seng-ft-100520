c1fba1617cc79b9e7b1c680e34fafb0209c0abcf
Matthew Sallin <msallin@gmail.com>
2020-09-28 23:03:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,7 @@ def big_shoe_rebounds()
     binding.pry
     sorted = playerArray.sort_by { |k| k[:shoe]) }
     binding.pry
-    return playerArray[:rebounds][0]
+    return sorted[:rebounds][0]
   end
 end
 
